20802ae42d75d36d9455c7e27f3efce9a8b6b3e0
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-07-09 17:17:16 +0200

    Fin de formation

--- my-app/cypress/integration/home/home.spec.js
+++ my-app/cypress/integration/home/home.spec.js
@@ -16,6 +16,10 @@ describe('home page', () => {
         // Exercice
         // Ensuite simuler le click sur Eric
         // Verifier que Eric s'affiche dans la TopBar et sur la Home
+        cy.get('[class^=SelectItem_host').last().click();
+
+        cy.get('.TopBar').should('contain.text', 'Eric');
+        cy.get('.App-Route-Container').should('contain.text', 'Vous avez sélectionné : Eric');
       })
   })
   
--- my-app/src/components/Select/Select.test.js
+++ my-app/src/components/Select/Select.test.js
@@ -5,17 +5,23 @@ import Select from './Select';
 it('renders without crashing', () => {
   const items = ['Acheter du pain', 'B', 'C'];
 
-  const wrapper = mount(<Select items={items} />);
+  const spy = jest.fn();
+
+  const wrapper = mount(<Select items={items} selected="C" onSelected={spy} />);
 
   // pour les CSS modules les classes commencent par NomDuComp_ClassOrigin__
   // wrapper.find('[class^=SelectItem_host]')
   expect(wrapper.contains('Acheter du pain')).toBeTruthy();
 
-  wrapper.find('[class^=Select_host]').simulate('click');
+  wrapper.find('.host').first().simulate('click');
+
 
   // Exercice
   // Ensuite simuler le click sur Acheter du pain
   // Passer une fonction créer avec jest.fn() à la props onSelected
   // vérifier que cette function soit appelée avec Acheter du pain
+  wrapper.find('.host .host').first().simulate('click');
+
+  expect(spy).toHaveBeenCalledWith('Acheter du pain');
 });
 
